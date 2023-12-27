0c924345cc650c088a201be944f56e3a364a4cc0
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2023-12-27 04:34:51 +0200

    dest setup

--- frontend/src/pages/destinations/Destinations.js
+++ frontend/src/pages/destinations/Destinations.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React from 'react';
+// import Welcome from '../../components/welcome/Welcome.js';
 
 export default function Destinations() {
   return (
-    <div>Destinations</div>
+    <div className='container'>
+    {/* <div className='row'>
+      <Welcome />
+    </div> */}
+    <div className='row create-blog-container'>
+      <div className='col-md-4'>
+        <div className=''>
+
+        </div>
+      </div>
+      <div className='col-md-8'>
+        <div className=''>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
