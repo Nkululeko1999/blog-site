b35897e113617b15c0de9b0a2b75fbf6cf2b4176
Nkululeko1999 <n.mkhatywa2019@gmail.com>
2023-12-27 02:58:16 +0200

    reset psd page

--- frontend/src/pages/forgot-password/ForgotPassword.js
+++ frontend/src/pages/forgot-password/ForgotPassword.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from 'react';
 
 export default function ForgotPassword() {
   return (
-    <div>ForgotPassword</div>
+    <div className='container mb-5'>
+    <div className='row justify-content-center'>
+        <div className='col-md-5 register-box p-3 my-4'>
+            <form class="form-register">
+                <div class="text-center form-header">
+                    <h2 className='mt-3'>Reset Password</h2>
+                </div>
+
+                <div className='divider'>
+                    <hr/>
+                </div>
+
+                <div class="form-label-group mt-5">
+                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" />
+                </div>
+
+                <button class="btn btn-lg btn-block mt-4" type="submit">Reset Password</button>
+            </form>
+        </div>
+    </div>
+</div>
   )
 }
